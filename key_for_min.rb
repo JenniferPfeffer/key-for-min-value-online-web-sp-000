d28b62a9c517fc6cf602eb86b146af6525ae799c
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-30 15:52:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value= 0 
   lowest_key = nil
   name_hash.find do |key, value| 
-    
+    if lowest_value == 0 || value < lowest_value 
+      
     
 end
