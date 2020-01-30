6939c490f7e2ef7c730b78807dc04c936150dbe1
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-30 15:51:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest value= 0 
+  lowest key = nil
   name_hash.find do |key, value| 
     
 end
